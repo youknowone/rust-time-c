e00625356313980a698a32ba56b382b6840c2a5a
Jeong YunWon <jeong@youknowone.org>
2024-08-12 20:15:36 +0900

    duration_since

--- src/rust_time.cxx
+++ src/rust_time.cxx
@@ -19,4 +19,9 @@ void rust_time_init() { rust::time::init(); }
 void rust_time_instant_now(rust::time::Instant *instant) {
   *instant = rust::time::Instant::now();
 }
+void rust_time_instant_duration_since(rust::time::Instant &lhs,
+                                      rust::time::Instant &rhs,
+                                      rust::time::Duration *duration) {
+  *duration = lhs.duration_since(rhs);
+}
 }
